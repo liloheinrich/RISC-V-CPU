308d3c08487417524c3f71dac4142061b201cd21
Lilo Heinrich <lheinrich@olin.edu>
2022-12-12 18:31:42 -0500

    push all_instructions.s

--- asm/all_instructions.s
+++ asm/all_instructions.s
@@ -1,5 +1,13 @@
 addi x1, x0, 15
-jal x6, label_a
-addi x1, x0, 16
-label_a: addi x1, x0, 17
-jalr sp, x6, 0
+addi x2, x0, 15
+beq x1, x2, label_a
+addi x3, x0, 16
+label_a: addi x4, x0, 17
+
+
+
+#addi x1, x0, 15
+#jal x6, label_a
+#addi x1, x0, 16
+#label_a: addi x1, x0, 17
+#jalr sp, x6, 0
